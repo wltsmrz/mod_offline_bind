91663c65ab505693b7c8126b8a1d5a525bb70025
wltsmrz <chlavois@gmail.com>
2013-04-06 19:17:16 +0000

    Initial commit

---
+++ README.md
@@ -0,0 +1,15 @@
+#mod_offline_authed
+
+This is an alternative to the `offline` module for [Prosody](http://prosody.im/) XMPP server. It has the same functionality; if a user is offline when messages are sent, they [the messages] will be stored in an offline queue and resent when the user re-appears. This module differs from the `presence` module in that, the `offline` module is dependent on `presence` module to send `message/offline/broadcast` event. This module, `mod_offline_authed` is independent of `presence`, and listens for `authentication-success` instead. It works. And it allows you to disable presence and keep your offline message functionality.
+
+###Note
+
++ This module is not tested on previous versions, but it's simple enough that it either should "just work" or "just work with minor tweaking". It probably works.
++ This module requires that `mod_saslauth` is enabled, as it listens for authentication events dispatched from there.
+
+###Usage
+
++ Clone this repository into your `plugins` directory.
++ Disable `offline` module in your `prosody.cfg.lua`
++ Replace it with `offline_authed` 
++ Reload config or restart Prosody server
---
+++ mod_offline_authed.lua
@@ -0,0 +1,56 @@
+local st          = require "stanza";
+local datamanager = require "util.datamanager";
+local datetime    = require "util.datetime";
+local jid_split   = require "util.jid".split;
+
+module:hook("message/offline/handle", function(event)
+  local origin, stanza = event.origin, event.stanza;
+  local to = stanza.attr.to;
+  local node, host;
+
+  if to then
+    node, host = jid_split(to)
+  else
+    node, host = origin.username, origin.host;
+  end
+
+  stanza.attr.stamp, stanza.attr.stamp_legacy = datetime.datetime(), datetime.legacy();
+  local result = datamanager.list_append(node, host, "offline", st.preserialize(stanza));
+  stanza.attr.stamp, stanza.attr.stamp_legacy = nil, nil;
+
+  return result;
+end);
+
+module:hook("message/offline/broadcast", function(event)
+  local origin = event.origin;
+	local node, host = origin.username, origin.host;
+
+	local data = datamanager.list_load(node, host, "offline");
+	if not data then return true end
+
+	for _, stanza in ipairs(data) do
+		stanza = st.deserialize(stanza);
+		stanza:tag("delay", {
+      xmlns = "urn:xmpp:delay",
+      from = host,
+      stamp = stanza.attr.stamp
+    }):up(); -- XEP-0203
+
+		stanza:tag("x", {
+      xmlns = "jabber:x:delay",
+      from = host,
+      stamp = stanza.attr.stamp_legacy
+    }):up(); -- XEP-0091 (deprecated)
+
+		stanza.attr.stamp, stanza.attr.stamp_legacy = nil, nil;
+		origin.send(stanza);
+	end
+
+	datamanager.list_store(node, host, "offline", nil);
+
+	return true;
+end);
+
+module:hook("authentication-success", function(event)
+  module:fire_event("message/offline/broadcast", { origin = event.session });
+end, 999);
